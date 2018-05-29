d269feec364ba7f01cf67508216863595db3ac08
Daniel Estrada <isc.daniel.estrada@gmail.com>
2018-05-28 22:46:15 -0500

    por mientras...

--- src/proyecto/busqueda/Hash.java
+++ src/proyecto/busqueda/Hash.java
@@ -2,5 +2,13 @@ package proyecto.busqueda;
 
 public class Hash {
     
+    private int[] arreglo;
     
+    public Hash() {
+        
+    }
+    
+    public int hash(int clave) {
+        return 0;
+    }
 }
